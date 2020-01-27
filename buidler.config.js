46dcf96ca7acb42fe4bd8a884d637272bd94ef69
José Nogueira <josej@bndes.gov.br>
2020-01-27 14:50:35 -0300

    rinkeby config

--- buidler.config.js
+++ buidler.config.js
@@ -11,5 +11,23 @@ task("accounts", "Prints the list of accounts", async () => {
 });
 
 module.exports = {
-  defaultNetwork: "buidlerevm"
+  defaultNetwork: "rinkeby", //"buidlerevm"
+  networks: {
+    rinkeby: {
+      url: "https://rinkeby.infura.io/",
+      host: "vrt1281", // Connect to geth on the specified
+      port: 9545,
+      from: "0xd636349f5d7e03037e0f224c9b1ac3ccf104f4a5", // default address to use for any transaction Truffle makes during migrations
+      network_id: 4,
+      gas: 6952388  // Gas limit used for deploys
+    },
+    ropsten: {
+      url: "https://ropsten.infura.io/",
+      host: "localhost",  //geth --testnet --fast --rpc --rpcapi eth,net,web3,personal
+      port:  8545,
+      from: "0x5a2a2ba72133d6667a9abcc1bc882125904cb88a", // owner BNDESToken na Ropsten
+      network_id: 3,
+      gas: 4612388
+    }    
+  }
 };
